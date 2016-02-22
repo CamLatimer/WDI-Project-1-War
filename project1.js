2edc8bf3942498e55cbcf50fe158cfc22827d925
Cam <camlatimer@gmail.com>
2016-02-22 10:51:55 -0500

    commit 0 -- added files to new branch. continue working on plans

---
+++ project1.js
@@ -0,0 +1,21 @@
+console.log('scripts up...')
+/*
+game object that contains
+a deck
+  deck is an array of card objects
+  card object properties
+    cardName
+    cardWorth
+function to shuffle the deck
+function to build players
+  each player is an object
+  player object properties
+    playerName
+    playerDeck
+      array of cards made from game.deck
+function to deal cards to players
+  divides deck into arrays for each player
+  assigns each new deck to a player's player.playerDeck property
+function to check to see which player's card is highest
+function to start or end game
+*/
