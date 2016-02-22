a46be5ed3de89ca9f5f6c433cc6cd28d8ecfc674
Cam <camlatimer@gmail.com>
2016-02-22 11:31:23 -0500

    commit1

--- project1.js
+++ project1.js
@@ -16,6 +16,28 @@ function to build players
 function to deal cards to players
   divides deck into arrays for each player
   assigns each new deck to a player's player.playerDeck property
-function to check to see which player's card is highest
+function to playCard
+  compares the cards at the beginning of each players' deck
+  the player w/ highest card gets its beginning card and the other players' beginng card pushed to the end of his deck
 function to start or end game
+function to play()
+  while both players have more than 0 cards
+  if one player has 0 cards, end game
 */
+
+//build deck
+var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+values.forEach(function(val) {
+  suits.forEach(function(suit) {
+    game.warDeck.push(
+      value: val;
+      suit: suit;
+      worth: values.indexOf(value);
+    );
+  })
+})
+//game object
+var game = {
+  warDeck: = [];
+}
