7f9e09f68bf97a37efaf2217e47f3af0164ba6a3
Cam <camlatimer@gmail.com>
2016-02-22 16:35:35 -0500

    commit 1 -- took way too long to commit. i know

--- project1.js
+++ project1.js
@@ -28,16 +28,149 @@ function to play()
 //build deck
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
-values.forEach(function(val) {
-  suits.forEach(function(suit) {
-    game.warDeck.push(
-      value: val;
-      suit: suit;
-      worth: values.indexOf(value);
-    );
-  })
-})
+//some players to add to game automatically for testing
+var playersToAdd = ['cam', 'leah'];
+
 //game object
 var game = {
-  warDeck: = [];
+  warDeck: [],
+  players: [],
+  stage: [],
+  buildDeck: function() {
+    var self = this;
+    values.forEach(function(val) {
+      suits.forEach(function(suit) {
+        self.warDeck.push(
+          {
+          value: val,
+          suit: suit
+          }
+        );
+      })
+    })
+    self.warDeck.forEach(function(val){
+      val.worth = self.warDeck.indexOf(val);
+    })
+    console.log('og deck: ');
+    console.log(self.warDeck)
+    return self.warDeck;
+  },
+  shuffleDeck: function() {
+    var self = this;
+    self.warDeck.forEach(function(card) {
+      self.warDeck.sort(function(cardOne, cardTwo) {
+        if(Math.random() > 0.5) {
+          return 1
+        } else {
+          return -1;
+        }
+     })
+   })
+   console.log('shuffled deck: ');
+   console.log(self.warDeck);
+   return self.warDeck;
+ },
+ buildPlayers: function() {
+   var self = this;
+   playersToAdd.forEach(function(val) {
+     self.players.push({
+       playerName: val,
+       playerDeck: []
+     });
+   })
+   console.log('players: ');
+   console.log(self.players);
+ },
+ deal: function() {
+   var self = this;
+  //give a deck to each player
+    //divides deck into arrays for each player
+  var numDecks = self.players.length;
+  var numCards = self.warDeck.length;
+  var cardsPerDeck = numCards / numDecks;
+  var playerDeckHolder = [];
+  //makes a deck for each player
+  for(var a = 0; a < numDecks; a++) {
+    playerDeckHolder[a] = [];
+  }
+  //puts cards from warDeck into each deck
+  playerDeckHolder.forEach(function(val) {
+    for(var b = 0; b < cardsPerDeck; b++) {
+      val.push(self.warDeck.pop());
+    }
+  })
+  //give each player a deck
+  for(var c = 0; c < self.players.length; c++) {
+    self.players[c].playerDeck = playerDeckHolder[c];
+  }
+  //mark each card according to what player it's held in so you can keep track of who wins each round
+  self.players.forEach(function(val) {
+    val.playerDeck.forEach(function(index) {
+      index.cardMark = val.playerName;
+    })
+  });
+
+  //playerDecks are full
+  console.log('player decks: ')
+  console.log(playerDeckHolder);
+  //og warkDeck is now empty
+  console.log('war deck: ')
+  console.log(self.warDeck);
+  //check that each player has a deck
+  console.log('players have decks: ')
+  console.log(self.players);
+
+ },
+ fight: function() {
+   var self = this;
+   //function to pit cards against each other
+      //put a card from each player into play area. card comes from the beginning of each players deck
+  self.players.forEach(function(val) {
+    //puts one card from front of each players deck
+    self.stage.push(val.playerDeck.shift());
+  });
+    console.log('stage area: ');
+    console.log(self.stage);
+     //compares the cards put into the stage and returns highest card
+  return self.stage.sort(function(cardA, cardB) {
+            return cardA.worth - cardB.worth;
+          });
+  //show stage sorted:
+  console.log('stage sorted: ');
+  console.log(self.stage);
+  //round winner
+  console.log('round winner: ');
+  console.log(self.stage[1]);
+     //the player w/ highest card gets its beginning card and the other players' beginng card pushed //to the end of his deck
+ },
+ proDecks: function() {
+   var self = this;
+   var stageFix = self.stage;
+   console.log('new stage: ' + stageFix);
+   //depending on the cardMark in the higher ranked card, choose player to add card to
+   //get stage[1]'s cardMark
+   var roundWinner = stageFix[1].cardMark;
+   console.log('round winner is: ' + roundWinner);
+   //use cardMark to put both cards into the deck of the player with that cardMark
+   self.players.forEach(function(val) {
+     if(val.playerName.includes(roundWinner)) {
+       self.stage.forEach(function(a) {
+         val.playerDeck.push(self.stage.pop());
+       });
+       console.log('stage area: ');
+       console.log(self.stage);
+       console.log(val.playerName + "'s deck:" );
+       console.log(val.playerDeck);
+     }
+   })
+
+ },
+ play: function() {
+   this.buildDeck();
+   this.shuffleDeck();
+   this.buildPlayers();
+   this.deal();
+   this.fight();
+   this.proDecks();
+ }
 }
